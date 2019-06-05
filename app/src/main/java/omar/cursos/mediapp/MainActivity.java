d01a7b2cd9178f4a080065c92bb203b1230c9de3
omarSuarezRodriguez <51038495+omarSuarezRodriguez@users.noreply.github.com>
2019-06-05 12:59:55 -0500

    Commit

--- app/src/main/java/omar/cursos/mediapp/MainActivity.java
+++ app/src/main/java/omar/cursos/mediapp/MainActivity.java
@@ -1,5 +1,6 @@
 package omar.cursos.mediapp;
 
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+
+
+
+
+
+       // bottomNav.disableShiftMode(bottomNavigationView);
+
+
+
     }
+
+
+
 }
