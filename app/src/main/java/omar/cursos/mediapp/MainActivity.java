bd4e21700a7845c5e3a1935e6c5b0df9b48ce5d2
omarSuarezRodriguez <51038495+omarSuarezRodriguez@users.noreply.github.com>
2019-07-29 22:15:08 -0500

    Commit

--- app/src/main/java/omar/cursos/mediapp/MainActivity.java
+++ app/src/main/java/omar/cursos/mediapp/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main); //Hola
+        setContentView(R.layout.activity_main);
 
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
                 new HomeFragment()).commit();
